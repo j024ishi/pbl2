3d2c6923acd2d2d73fe96c376d423d4380944c60
j024ishi <j520024k@mails.cc.ehime-u.ac.jp>
2024-12-09 11:01:27 +0900

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,8 +1,35 @@
 <?php
-// ユーザーIDを取得
-$userID = $_GET['userID'] ?? '';
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['login'])==false)
+{
+    print 'ログインされていません<br/>';
+    print '<a href="login.html">ログイン画面へ</a>';
+    exit();
+}
+else
+{
+    $userID=$_SESSION['userID'];
+    $name=$_SESSION['name'];
+}
+
 
 // ページ内容を表示
 echo "<h1>ようこそ ユーザーID: $userID さん</h1>";
-echo "<p>あなた専用のページです。</p>";
+print $name;
+echo "さんのページです。";
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+
+<br><br>
+<button onclick="location.href='logout.php'">ログアウト</button>
+
+</body>
+<html>
